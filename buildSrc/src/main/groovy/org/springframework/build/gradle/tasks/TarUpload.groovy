8d7603a30cd556e1b84d58a2778679b9341de348
Chris Beams <cbeams@gmail.com>
2010-07-11 18:43:12 +0200

    documentation polish for buildSrc artifacts; pushed RabbitUtils content into build.gradle and eliminated the class

--- buildSrc/src/main/groovy/org/springframework/build/gradle/tasks/TarUpload.groovy
+++ buildSrc/src/main/groovy/org/springframework/build/gradle/tasks/TarUpload.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.build.gradle.tasks;
 
 import org.gradle.api.DefaultTask;
@@ -6,8 +22,11 @@ import org.gradle.api.tasks.bundling.Tar;
 import org.gradle.api.tasks.bundling.Compression;
 
 /**
- * Extends the Tar task, uploading the created archive to a remote directory, unpacking and deleting it.
- * Requires Ant ssh (jsch) support.
+ * Extends the Tar task, uploading the created archive to a remote directory,
+ * unpacking and deleting it.  Requires Ant ssh (jsch) support.
+ *
+ * @author ltaylor
+ * @author cbeams
  */
 class TarUpload extends Tar {
     @Input
@@ -34,7 +53,7 @@ class TarUpload extends Tar {
         String host = login.host
 
         project.ant {
-            // TODO: hook in a listener via Jsch, or ivy resolver, etc.
+            // TODO: get progress metrics by hooking in a listener via Jsch, or ivy resolver, etc.
             taskdef(name: 'scp',
                 classname: 'org.apache.tools.ant.taskdefs.optional.ssh.Scp', classpath: classpath.asPath)
             taskdef(name: 'sshexec',
