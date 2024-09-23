4920e266af1c471427c85a0dc069cf7052ff39e8
zhoufudun <1215906004@qq.com>
2024-09-24 00:20:03 +0800

    read

--- springboot-mq-demo/publisher/src/main/java/com/itheima/publisher/PublisherApplication.java
+++ springboot-mq-demo/publisher/src/main/java/com/itheima/publisher/PublisherApplication.java
@@ -1,5 +1,6 @@
 package com.itheima.publisher;
 
+
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
